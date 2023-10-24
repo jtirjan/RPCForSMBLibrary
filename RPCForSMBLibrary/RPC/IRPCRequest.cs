101c679c2f0fa9eda75d0cb38fa2bcb11cd21d59
Jon Tirjan <JTirjan@skysync.com>
2023-10-24 15:17:01 -0500

    Update Framework & Add Missing Interface

---
+++ RPCForSMBLibrary/RPC/IRPCRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMBLibrary.RPC
+{
+    public interface IRPCRequest
+    {
+        public byte[] GetBytes();
+    }
+}
